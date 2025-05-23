8109d57d2c74b77e1fc2004b6058dcec90f422b1
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-23 12:34:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const delayedPromise = (delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, delay);
+  });
+};
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const text = document.getElementById("text")?.value;
+  const delay = document.getElementById("delay")?.value;
+  const output = document.getElementById("output");
+  try {
+    await delayedPromise(delay);
+    output.innerText=text;
+  } catch (err) {
+    output.innerText="";
+  }
+};
